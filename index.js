f2717425cc08af8e4b811a0f8e79a8851407f980
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-08 02:59:44 +0600

    Added Required Files

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+var cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.4oearpf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+  
+      const uploadDatabase = client.db('api-creation').collection('news-api');
+      app.get('/news-data', async (req, res) =>{
+        const query = {};
+        const cursor = uploadDatabase.find(query);
+        const data = await cursor.toArray();
+        res.send(data);
+      })
+  
+      app.post('/news-upload', (req, res) => {
+        const addData = req.body;
+        const result = uploadDatabase.insertOne(addData)
+        res.send(result)
+  
+      })
+    } finally {
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
