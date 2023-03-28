adec1121d47b1318fda4a80ed6afc168a3fec6e8
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-03-28 20:19:58 +0600

    Added Single News

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 var cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -16,12 +16,23 @@ async function run() {
   
       const newsCollection = client.db('api-creation').collection('news-api');
       const pcProductCollection = client.db('api-creation').collection('pc-product');
+
+      // GET ALL NEWS DATA 
       app.get('/news-data', async (req, res) =>{
         const query = {};
         const cursor = newsCollection.find(query);
         const data = await cursor.toArray();
         res.send(data);
       })
+
+      app.get('/news-data/:id', async (req, res) =>{
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const result = await newsCollection.findOne(query)
+        res.send(result)
+      })
+
+      // GET ALL PC PRODUCT DATA 
       app.get('/pc-product', async (req, res) =>{
         const query = {};
         const cursor = pcProductCollection.find(query);
@@ -29,6 +40,7 @@ async function run() {
         res.send(data);
       })
   
+      // UPLOAD NEWS DATA 
       app.post('/news-upload', (req, res) => {
         const addData = req.body;
         const result = newsCollection.insertOne(addData)
